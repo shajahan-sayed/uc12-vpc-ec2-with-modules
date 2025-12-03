1de644292a065832a15c0e4a221b8133fe0037c4
shajahan-sayed <shajahansayed111@gmail.com>
2025-12-03 16:49:42 +0530

    Create output.tf

---
+++ modules/route-table/output.tf
@@ -0,0 +1,12 @@
+output "public1_route_table" {
+    value = aws_route_table.public1.id
+}
+output "public2_route_table" {
+    value = aws_route_table.public2.id
+}
+output "private1_route_table" {
+    value = aws_route_table.private1.id
+}
+output "private2_route_table" {
+    value = aws_route_table.private2.id
+}
